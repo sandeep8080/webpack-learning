b517c5ef4acf945789b46168187aba379ea56bae
Sandeep Garg <sandeepgarg14319@gmail.com>
2025-06-02 11:47:21 +0530

    chore: initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import "./index.css";
+
+const form = document.getElementById("add-form");
+const output = document.getElementById("result");
+
+function sum(nums) {
+  const result = nums.reduce((acc, num) => acc + num, 0);
+  return result;
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const { first, second } = e.target.elements;
+  const result = sum([+first.value, +second.value]);
+  output.innerText = `Total = ${result}`;
+});
+
+var foo = "Foo";
+function multiply(nums) {
+  const result = nums.reduce((acc, num) => acc * num, 1);
+  return result;
+}
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // To extract the CSS into a new chunk but this file is not optimized and is same as the origin css file.
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin"); // Use to minimize the above CSS file
+const TerserPlugin = require("terser-webpack-plugin"); // To minimize the js files
+const { PurgeCSSPlugin } = require("purgecss-webpack-plugin"); // Plugin to treeshake CSS
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "none",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        // When multiple loaders are specified in the use property, webpack applies them from right to left.
+        // Each loader in the chain performs a specific task or transformation on the source file and passes the result to the next loader.
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+    minimize: true, // To minimize the files in Development too.
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "bundle.css",
+    }),
+    new PurgeCSSPlugin({
+      paths: ["./src/index.html"],
+    }),
+  ],
+};
